db74805cbbad68b65bfefe653ffecec86cb6dc1b
AxelGenao <65257294+AxelGenao@users.noreply.github.com>
2023-01-03 17:22:45 -0400

    Add files via upload

---
+++ Console Screen Handler 3/ConsoleApp5/ConsoleApp5/Ganador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Formulario
+    {
+        public int ID{ get; set; }
+        public string Titulo {get; set; }
+        public List<Pregunta> Preguntas {get; set; }
+    }
+}
+
+public class Pregunta
+{
+    public string Texto { get; set; }
+    public bool Requerido { get; set; }
+}
